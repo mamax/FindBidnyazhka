227d11b140d2aabfc9aeabbb9d6396b3765096b3
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2016-02-25 15:20:18 +0200

    remove unuseful from commElements

--- src/main/java/ua/tcb/common/CommElements.java
+++ src/main/java/ua/tcb/common/CommElements.java
@@ -6,7 +6,6 @@ import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 
 import java.util.List;
-import java.util.Random;
 
 @Name("Common elements")
 @FindBy(xpath="//div[contains(@class,'main')]")
@@ -22,50 +21,10 @@ public class CommElements extends HtmlElement {
 
     private static final String BROWSER_FF = "firefox";
 //    protected WebDriver driver;
-    private boolean acceptNextAlert = true;
-    protected final static String pass = "123456";
-    protected final String ssn = "197910252399";
-    protected String baseUrl;
-    private static final String DEVTEST = "//src//main//resources//application.properties";
-
-    protected static String rndStrNum() {
-        char[] chars = "0123456789".toCharArray();
-        StringBuilder sb = new StringBuilder();
-        Random random = new Random();
-        for (int i = 0; i < 5; i++) {
-            char c = chars[random.nextInt(chars.length)];
-            sb.append(c);
-        }
-        return sb.toString();
-    }
-
-    protected static String rndStrName() {
-        char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        StringBuilder sb = new StringBuilder();
-        Random random = new Random();
-        for (int i = 0; i < 5; i++) {
-            char c = chars[random.nextInt(chars.length)];
-            sb.append(c);
-        }
-        return sb.toString();
-    }
-
-
 
     public void typeIn(WebElement element, String text) {
         element.clear();
         element.sendKeys(text);
-    }
-
-    public void typeIn(WebElement element, int  val) {
-
-        element.clear();
-        element.sendKeys(String.valueOf(val));
-    }
-
-    public void typeEnter(WebElement element, String text) {
-        typeIn(element, text);
-        element.sendKeys(Keys.ENTER);
     }
 
     protected boolean isPresentAndDisplayed(WebElement element) {
@@ -83,7 +42,6 @@ public class CommElements extends HtmlElement {
 //        } else
 //            return false;
 //    }
-//
 //
 //    protected boolean isLoggedIn() {
 //        if (isPresentAndDisplayed(logOutButton)) {
